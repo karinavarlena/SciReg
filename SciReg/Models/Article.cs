df4bf1c364e665d1334dfa9756c6466f9a14f1d3
karinavarlena <karina.varlena@gmail.com>
2020-04-10 23:19:44 +0300

    commit my project)))

---
+++ SciReg/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace SciReg.Models
+{
+    public class Article
+    {
+        public DateTime ReleaseDate { get; set; }
+        public String Title { get; set; }
+        public Subject ArticleSubject { get; set; }
+        public ContentType ArticleContentType { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public String ISSN { get; set; }
+        public Language ArticleLanguage { get; set; }
+        public String Text { get; set; }
+        public ApplicationUser Publisher { get; set; }
+        public ApplicationUser Author { get; set; }
+    }
+}
---
+++ SciReg/Models/ContentType.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SciReg.Models
+{
+    public class ContentType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
